d0914e0211cc437cf23bbcc6d762652659f9e2fe
MarcoPolo987 <49919375+MarcoPolo987@users.noreply.github.com>
2019-04-30 10:37:37 -0700

    Add files via upload

--- Mario.java
+++ Mario.java
@@ -1,44 +1,70 @@
+
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
 public class Mario extends Character{
+	private int x; private int y;
 	private static final int WIDTH=50,HEIGHT=50;
-	 private Rectangle rect;
-	 private Image img;
-	 public Mario(int x, int y) {
+	private Rectangle rect;
+	private static Image spriteSheet;
+	private static Image img;
+	public Mario(int x, int y) {
 		super(x,y);
-		try {
-			
-			img=ImageIO.read(this.getClass().getResource(".png"));
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
- }
+		//		 try {
+//
+//			 img=ImageIO.read(this.getClass().getResource(".png"));
+//		 } catch (IOException e1) {
+//			 // TODO Auto-generated catch block
+//			 e1.printStackTrace();
+//		 }
+	 }
+	 private static Image getImage() {
+		 openSpriteSheet();
+		 if(img == null)
+			 img= openImageFromSpriteSheet(50, 50, 50, 50);
+		 return img;
+	 }
+	 private static void openSpriteSheet() {
+			if(spriteSheet==null) {
+				try {
+					spriteSheet = ImageIO.read(new File("spritesheet.gif"));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	 protected static Image openImageFromSpriteSheet(int x, int y, int w, int h) {
+		 return ((BufferedImage)spriteSheet).getSubimage(x,y,w,h).getScaledInstance(64, 64, BufferedImage.SCALE_SMOOTH);
+
+	 }
 	 public Rectangle getRect() {
 		 return this.rect;
 	 }
-	 public void move(int x, int y) {
-		 rect.translate(x,y);
+	 public void move() {
+		 //rect.translate(x,y);
 	 }
-	 public void moveRight(int x, int y) {
-		 rect.translate(x+1,y);
+	 public  void moveRight() {
+		 //rect.translate(x+1,y);
+		 x+=10;
+		 System.out.println(x);
 	 }
-	 public void moveLeft(int x, int y) {
-		 rect.translate(x-1,y+1);
+	 public  void moveLeft() {
+		 //rect.translate(x-1,y+1);
+		 x-=10;
 	 }
-	 public void jump(int x,int y) {
-		 rect.translate(x, y-2);
+	 public  void jump() {
+		 y+=10;
+		 //rect.translate(x, y-2);
 	 }
-	 public void crouch(int x, int y) {
-		 rect.translate(x-2, y-2);
-	 }
-	 public void draw(Graphics g, int x, int y) {
-		 g.drawImage(img,x,y,WIDTH,HEIGHT, null); 
+	 public  void crouch() {
+		 y-=10;
+		 //rect.translate(x-2, y-2);
 	 }
 	 public boolean collide(EnemyObj e) {
 		 if (this.rect.intersects(e.getRect())){
@@ -47,8 +73,13 @@ public class Mario extends Character{
 		 return false;
 	 }
 	 public boolean powerUp(PowerUp p) {
-		 if (this.rect.intersect(p.getRect())) {
+		 if (this.rect.intersects(p.getRect())) {
 			 return true;
 		 }
+		return false;
 	 }
+	@Override
+	public void draw(Graphics g) {
+		g.drawImage(img, x, y, 64, 64, null);
+	}
 }
