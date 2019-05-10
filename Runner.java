68c8f62960a66e7363d8654a4e2347b69691ec24
MarcoPolo987 <49919375+MarcoPolo987@users.noreply.github.com>
2019-05-10 10:58:03 -0700

    Add files via upload

--- Runner.java
+++ Runner.java
@@ -8,11 +8,11 @@ public class Runner {
 	private JPanel panel;
 	private Game game = new Game();
 	private Timer timer;
-	private Mario mario = new Mario(5, 5);
+	private Mario mario = new Mario(155, 225);
 	private MysteryBlock m = new MysteryBlock(150, 300);
 	private Blocks blockss = new Blocks();
-	private Koopa k = new Koopa(150, 250);
-	private Bowser boss = new Bowser(300, 250);
+	private Koopa k = new Koopa(150, 450);
+	private Bowser boss = new Bowser(300, 450);
 	private int ticks, sx;
 	private Grounds grounds = new Grounds();
 	public static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -21,12 +21,23 @@ public class Runner {
 	public static void main(String[] args) {
 		new Runner().start();
 	}
+	protected void updateGame() {
+		ticks++;// keeps track of the number of times the timer has gone off
+		
+		int hurts = 1000/REFRESH_RATE;
+		if(ticks %hurts == 0) {
+			System.out.println(ticks/hurts+" seconds");
+		}
+	}
 	public void start() {
+		
 		ArrayList<Ground> ground = grounds.getGround(); 
 		ArrayList<Block> blocks = blockss.getBlocks(); 
 		JFrame frame = new JFrame("Mario!");
 		timer = new Timer(sx, null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		timer.start();
+		
 		panel = new JPanel() {
 			@Override
 			public void paintComponent(Graphics g) {
@@ -36,6 +47,7 @@ public class Runner {
 					block.draw(g);
 				}
 				m.draw(g);
+				k.draw(g);
 				//				pipe.draw(g);
 				//				arrow.draw(g);
 				//				button.draw(g);
@@ -44,20 +56,32 @@ public class Runner {
 				}
 			};	
 		};
-		timer.addActionListener(new ActionListener() {
+		timer = new Timer(REFRESH_RATE, new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) {
-				//System.out.println("timer went off!");
-//				for(Ground g : ground) {
-//					System.out.println(mario.stand(g));
-//					if(mario.stand(g)) {
-//						
-//					}
-//				}
-				//mario.move();
-				//panel.repaint();
+			public void actionPerformed(ActionEvent arg0) {
+				updateGame();
+				mario.move();
+				k.move(mario);
+				panel.repaint();
 			}
 		});
+		timer.start();
+//		timer.addActionListener(new ActionListener() {
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				updateGame();
+//				System.out.println("timer went off!");
+//				mario.move();
+//				k.move(mario);
+//				panel.repaint();
+////				for(Ground g : ground) {
+////				System.out.println(mario.stand(g));
+////				if(mario.stand(g)) {
+////					
+////				}
+////			}
+//			}
+//		});
 		panel.setPreferredSize(new Dimension(800,550));
 		frame.add(panel);
 		frame.pack();
@@ -107,10 +131,11 @@ public class Runner {
 				hit("space");
 			}
 		});
+		
 	}
 	public  void hit(String s) {
 		//System.out.println(mario.getX()+""+ mario.getY());
-		timer.start();
+		
 		keyHit(s);
 		panel.repaint();
 	}
